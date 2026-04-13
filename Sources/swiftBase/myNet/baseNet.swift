0da13c4b81cb0639daa9ad912eea3a4bc0ee98bd
jinguihua <156711203@qq.com>
2026-04-13 14:55:17 +0800

    changed

--- Sources/swiftBase/myNet/baseNet.swift
+++ Sources/swiftBase/myNet/baseNet.swift
@@ -88,3 +88,13 @@ public actor NetBusinessHandler {
 
 public enum myNet {
 }
+
+
+//MARK: 响应解析
+public protocol MyNetResponseParser: Sendable {
+    func parse<T: Decodable>(
+        data: Data,
+        httpCode: Int,
+        decoder: JSONDecoder
+    ) throws -> T
+}
